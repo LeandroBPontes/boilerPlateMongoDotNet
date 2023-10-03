db634549d600e8189f9a60c5bf83e1de16933e01
Leandro Barros Pontes <120100904+leandroBuiltCode@users.noreply.github.com>
2023-10-03 16:10:43 -0300

    init up

---
+++ WebApiMongoDB/Controllers/ProdutosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiMongoDB.Models;
+using WebApiMongoDB.Services;
+
+namespace WebApiMongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ProdutoServices _produtoServices;
+
+        public ProdutosController(ProdutoServices produtoServices)
+        {
+            _produtoServices = produtoServices;
+        }
+
+        [HttpGet]
+        public async Task<List<Produto>> GetProdutos()
+            => await _produtoServices.GetAsync();
+
+        [HttpPost]
+        public async Task<Produto> PostProduto(Produto produto)
+        {
+            await _produtoServices.CreateAsync(produto);
+
+            return produto;
+        }
+        
+        [HttpDelete]
+        public async Task<NoContentResult> DeleteProduto(string produtoId)
+        {
+            await _produtoServices.RemoveAsync(produtoId);
+
+            return new NoContentResult();
+        }
+    }
+}
